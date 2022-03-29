e62c6f69dd34a4a2cff77d9040c347718320248a
Arthur Picht <picht@arthurpicht.de>
2022-03-29 17:22:18 +0200

    formatted and colorized output

--- src/main/java/org/mentalizr/infra/executors/StatusExecutor.java
+++ src/main/java/org/mentalizr/infra/executors/StatusExecutor.java
@@ -1,5 +1,7 @@
 package org.mentalizr.infra.executors;
 
+import com.diogonunes.jcolor.Ansi;
+import com.diogonunes.jcolor.Attribute;
 import de.arthurpicht.cli.CliCall;
 import de.arthurpicht.cli.CommandExecutor;
 import de.arthurpicht.cli.CommandExecutorException;
@@ -14,36 +16,50 @@ import org.mentalizr.infra.buildEntities.ports.PortMongo;
 import org.mentalizr.infra.buildEntities.ports.PortTomcat;
 import org.mentalizr.infra.docker.m7r.*;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class StatusExecutor implements CommandExecutor {
 
     private static final int minLengthString = 33;
+
+    private static final String UP = Ansi.colorize("UP", Attribute.GREEN_TEXT());
+    private static final String DOWN = Ansi.colorize("--", Attribute.RED_TEXT());
+    private static final String RUNNING = Ansi.colorize("RUNNING", Attribute.GREEN_TEXT());
+    private static final String STOPPED = Ansi.colorize("STOPPED", Attribute.RED_TEXT());
+    private static final String OPEN = Ansi.colorize("OPEN", Attribute.GREEN_TEXT());
+    private static final String CLOSED = Ansi.colorize("CLOSED", Attribute.RED_TEXT());
+    private static final String SUCCESS = Ansi.colorize("SUCCESS", Attribute.GREEN_TEXT());
+    private static final String SKIPPED = Ansi.colorize("SKIPPED", Attribute.YELLOW_TEXT());
+    private static final String FAILED = Ansi.colorize("FAILED", Attribute.RED_TEXT());
+
 
     @Override
     public void execute(CliCall cliCall) throws CommandExecutorException {
         ExecutionContext.initialize(cliCall);
 
-        System.out.println("mentalizr infrastructure status");
+        System.out.println("mentalizr infrastructure status on " + Ansi.colorize(getHostname(), Attribute.WHITE_TEXT(), Attribute.BOLD()));
 
         String networkString = Strings.fillUpAfter("Network [" + Const.NETWORK + "]: ", ' ', minLengthString);
         if (M7rNetwork.exists()) {
-            System.out.println(networkString + "UP");
+            System.out.println(networkString + UP);
         } else {
-            System.out.println(networkString + "--");
+            System.out.println(networkString + DOWN);
         }
 
         String volumeMongoString = Strings.fillUpAfter("MongoDB volume [" + Const.VOLUME_MONGO + "]: ", ' ', minLengthString);
         if (M7rVolumeMongo.exists()) {
-            System.out.println(volumeMongoString + "UP");
+            System.out.println(volumeMongoString + UP);
         } else {
-            System.out.println(volumeMongoString + "--");
+            System.out.println(volumeMongoString + DOWN);
         }
 
         String containerMongoString = Strings.fillUpAfter("MongoDB container [" + Const.CONTAINER_MONGO + "]: ", ' ', minLengthString);
         if (M7rContainerMongo.exists()) {
             if (M7rContainerMongo.isRunning()) {
-                System.out.println(containerMongoString + "UP Running");
+                System.out.println(containerMongoString + UP + " " + RUNNING);
             } else {
-                System.out.println(containerMongoString + "UP Stopped");
+                System.out.println(containerMongoString + UP + " " + STOPPED);
             }
         } else {
             System.out.println(containerMongoString + "--");
@@ -52,116 +68,125 @@ public class StatusExecutor implements CommandExecutor {
         String portMongoString = Strings.fillUpAfter("MongoDB port 27017: ", ' ', minLengthString);
         boolean mongoPortIsListening = PortMongo.isListening();
         if (mongoPortIsListening) {
-            System.out.println(portMongoString + "OPEN");
+            System.out.println(portMongoString + OPEN);
         } else {
-            System.out.println(portMongoString + "CLOSED");
+            System.out.println(portMongoString + CLOSED);
         }
 
         String connectionMongoString = Strings.fillUpAfter("MongoDB probe client connection: ", ' ', minLengthString);
         System.out.print(connectionMongoString);
         if (mongoPortIsListening) {
             if (ConnectionMongo.connectionSuccessful()) {
-                System.out.println("SUCCESS");
+                System.out.println(SUCCESS);
             } else {
-                System.out.println("FAILED");
+                System.out.println(FAILED);
             }
         } else {
-            System.out.println("SKIPPED");
+            System.out.println(SKIPPED);
         }
 
         String volumeMariaString = Strings.fillUpAfter("MariaDB volume [" + Const.VOLUME_MARIA + "]: ", ' ', minLengthString);
         if (M7rVolumeMaria.exists()) {
-            System.out.println(volumeMariaString + "UP");
+            System.out.println(volumeMariaString + UP);
         } else {
-            System.out.println(volumeMariaString + "--");
+            System.out.println(volumeMariaString + DOWN);
         }
 
         String containerMariaString = Strings.fillUpAfter("MariaDB container [" + Const.CONTAINER_MARIA + "]: ", ' ', minLengthString);
         if (M7rContainerMaria.exists()) {
             if (M7rContainerMaria.isRunning()) {
-                System.out.println(containerMariaString + "UP Running");
+                System.out.println(containerMariaString + UP + " " + RUNNING);
             } else {
-                System.out.println(containerMariaString + "UP Stopped");
+                System.out.println(containerMariaString + UP + " " + STOPPED);
             }
         } else {
-            System.out.println(containerMariaString + "--");
+            System.out.println(containerMariaString + DOWN);
         }
 
         String portMariaString = Strings.fillUpAfter("MariaDB port 3306: ", ' ', minLengthString);
         boolean mariaPortIsListening = PortMaria.isListening();
         if (mariaPortIsListening) {
-            System.out.println(portMariaString + "OPEN");
+            System.out.println(portMariaString + OPEN);
         } else {
-            System.out.println(portMariaString + "CLOSED");
+            System.out.println(portMariaString + CLOSED);
         }
 
         String connectionMariaString = Strings.fillUpAfter("MariaDB probe client connection: ", ' ', minLengthString);
         System.out.print(connectionMariaString);
         if (mariaPortIsListening) {
             if (ConnectionMaria.probe()) {
-                System.out.println("SUCCESS");
+                System.out.println(SUCCESS);
             } else {
-                System.out.println("FAILED");
+                System.out.println(FAILED);
             }
         } else {
-            System.out.println("SKIPPED");
+            System.out.println(SKIPPED);
         }
 
         String volumeTomcatString = Strings.fillUpAfter("Tomcat volume [" + Const.VOLUME_TOMCAT + "]: ", ' ', minLengthString);
         if (M7rVolumeTomcat.exists()) {
-            System.out.println(volumeTomcatString + "UP");
+            System.out.println(volumeTomcatString + UP);
         } else {
-            System.out.println(volumeTomcatString + "--");
+            System.out.println(volumeTomcatString + DOWN);
         }
 
         String containerTomcatString = Strings.fillUpAfter("Tomcat container [" + Const.CONTAINER_TOMCAT + "]: ", ' ', minLengthString);
         if (M7rContainerTomcat.exists()) {
             if (M7rContainerTomcat.isRunning()) {
-                System.out.println(containerTomcatString + "UP Running");
+                System.out.println(containerTomcatString + UP + " " + RUNNING);
             } else {
-                System.out.println(containerTomcatString + "UP Stopped");
+                System.out.println(containerTomcatString + UP + " " + STOPPED);
             }
         } else {
-            System.out.println(containerTomcatString + "--");
+            System.out.println(containerTomcatString + DOWN);
         }
 
         String portTomcatString = Strings.fillUpAfter("Tomcat port 8080: ", ' ', minLengthString);
         boolean tomcatPortIsListening = PortTomcat.isListening();
         if (tomcatPortIsListening) {
-            System.out.println(portTomcatString + "OPEN");
+            System.out.println(portTomcatString + OPEN);
         } else {
-            System.out.println(portTomcatString + "CLOSED");
+            System.out.println(portTomcatString + CLOSED);
         }
 
         String connectionTomcatString = Strings.fillUpAfter("Tomcat probe web content: ", ' ', minLengthString);
         System.out.print(connectionTomcatString);
         if (tomcatPortIsListening) {
             if (ConnectionTomcat.probe()) {
-                System.out.println("SUCCESS");
+                System.out.println(SUCCESS);
             } else {
-                System.out.println("FAILED");
+                System.out.println(FAILED);
             }
         } else {
-            System.out.println("SKIPPED");
+            System.out.println(SKIPPED);
         }
 
         String containerNginxString = Strings.fillUpAfter("nginx container [" + Const.CONTAINER_NGINX + "]: ", ' ', minLengthString);
         if (M7rContainerNginx.exists()) {
             if (M7rContainerNginx.isRunning()) {
-                System.out.println(containerNginxString + "UP Running");
+                System.out.println(containerNginxString + UP + " " + RUNNING);
             } else {
-                System.out.println(containerNginxString + "UP Stopped");
+                System.out.println(containerNginxString + UP + " " + STOPPED);
             }
         } else {
-            System.out.println(containerNginxString + "--");
+            System.out.println(containerNginxString + DOWN);
         }
 
         String portNginxString = Strings.fillUpAfter("nginx port 443: ", ' ', minLengthString);
         boolean nginxPortIsListening = PortTomcat.isListening();
         if (nginxPortIsListening) {
-            System.out.println(portNginxString + "OPEN");
+            System.out.println(portNginxString + OPEN);
         } else {
-            System.out.println(portNginxString + "CLOSED");
+            System.out.println(portNginxString + CLOSED);
+        }
+
+    }
+
+    private static String getHostname() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "UNKNOWN";
         }
 
     }
--- src/main/java/org/mentalizr/infra/tasks/InfraTaskRunner.java
+++ src/main/java/org/mentalizr/infra/tasks/InfraTaskRunner.java
@@ -1,9 +1,12 @@
 package org.mentalizr.infra.tasks;
 
+import com.diogonunes.jcolor.Ansi;
+import com.diogonunes.jcolor.Attribute;
 import de.arthurpicht.cli.CliCall;
 import de.arthurpicht.taskRunner.TaskRunner;
 import de.arthurpicht.taskRunner.runner.TaskRunnerBuilder;
 import de.arthurpicht.taskRunner.taskRegistry.TaskRegistry;
+import de.arthurpicht.utils.core.strings.Strings;
 import org.mentalizr.infra.InfraCli;
 
 public class InfraTaskRunner {
@@ -14,27 +17,27 @@ public class InfraTaskRunner {
         return new TaskRunnerBuilder()
                 .withTaskRegistry(taskRegistry)
                 .withOnPreExecuteCallback(task -> {
-                    System.out.print("[" + task.getName() + "] ... ");
+                    System.out.print(Strings.fillUpAfter("[" + task.getName() + "] ... ", ' ', 33));
                 })
                 .withOnSuccessCallback(task -> {
-                    System.out.println("OK");
+                    System.out.println(Ansi.colorize("OK", Attribute.GREEN_TEXT()));
                 })
                 .withOnSkipCallback(task -> {
-                    System.out.println("SKIPPED");
+                    System.out.println(Ansi.colorize("SKIPPED", Attribute.GREEN_TEXT()));
                 })
                 .withOnUpToDateCallback(task -> {
-                    System.out.println("UP-TO-DATE");
+                    System.out.println(Ansi.colorize("UP-TO-DATE", Attribute.GREEN_TEXT()));
                 })
                 .withOnFailByTaskPreconditionExceptionCallback((task, taskPreconditionException) -> {
-                    System.out.println("Precondition failed: " + taskPreconditionException.getMessage());
+                    System.out.println(Ansi.colorize("Precondition failed: ", Attribute.RED_TEXT()) + taskPreconditionException.getMessage());
                     if (showStacktrace) taskPreconditionException.printStackTrace();
                 })
                 .withOnFailByTaskExecutionExceptionCallback((task, taskExecutionException) -> {
-                    System.out.println("Error: " + taskExecutionException.getMessage());
+                    System.out.println(Ansi.colorize("Error: ", Attribute.RED_TEXT()) + taskExecutionException.getMessage());
                     if (showStacktrace) taskExecutionException.printStackTrace();
                 })
                 .withOnFailByRuntimeExceptionCallback((task, runtimeException) -> {
-                    System.out.println("RuntimeError: " + runtimeException.getMessage());
+                    System.out.println(Ansi.colorize("RuntimeError: ", Attribute.RED_TEXT()) + runtimeException.getMessage());
                     if (showStacktrace) runtimeException.printStackTrace();
                 })
                 .build();
