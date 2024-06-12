25706ba79612bb233516e4efcefe57edbcb1b7ea
Arthur Picht <picht@arthurpicht.de>
2024-06-12 15:23:51 +0200

    fixes after removing deprecated class utils/Timestamps

--- src/main/java/org/mentalizr/infra/docker/m7r/BackupTags.java
+++ src/main/java/org/mentalizr/infra/docker/m7r/BackupTags.java
@@ -1,6 +1,6 @@
 package org.mentalizr.infra.docker.m7r;
 
-import de.arthurpicht.utils.core.strings.Timestamps;
+import de.arthurpicht.utils.core.dates.TimestampStrings;
 import org.mentalizr.infra.ExecutionContext;
 
 import java.time.Instant;
@@ -10,9 +10,9 @@ public class BackupTags {
     public static String getNameWithBackupTag(String taggedImageName) {
         Instant callTimestamp = ExecutionContext.getCallTimestamp();
         if (taggedImageName.contains(":")) {
-            return taggedImageName + "-backup-" + Timestamps.dateHyphenTime(callTimestamp);
+            return taggedImageName + "-backup-" + TimestampStrings.asHyphenTime(callTimestamp);
         } else {
-            return taggedImageName + ":backup-" + Timestamps.dateHyphenTime(callTimestamp);
+            return taggedImageName + ":backup-" + TimestampStrings.asHyphenTime(callTimestamp);
         }
     }
 
